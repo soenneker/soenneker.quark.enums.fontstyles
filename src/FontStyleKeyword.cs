c8c875801b8e1515dfbba305ad6b821b018e5f38
Jake Soenneker <jake@soeneker.com>
2026-02-26 08:49:49 -0600

    Enum

--- src/FontStyleKeyword.cs
+++ src/FontStyleKeyword.cs
@@ -1,11 +1,11 @@
-using Intellenum;
+using Soenneker.Gen.EnumValues;
 
 namespace Soenneker.Quark.Enums;
 
 /// <summary>
 /// An HTML CSS in a .NET enumeration form
 /// </summary>
-[Intellenum<string>]
+[EnumValue<string>]
 public partial class FontStyleKeyword
 {
     /// <summary>
