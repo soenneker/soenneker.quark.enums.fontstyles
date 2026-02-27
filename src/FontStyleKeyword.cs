9fd887f4ffe453ccc5978dce6e116167b770746f
Jake Soenneker <jake@soenneker.com>
2026-02-27 08:25:55 -0600

    Deps

--- src/FontStyleKeyword.cs
+++ src/FontStyleKeyword.cs
@@ -6,6 +6,7 @@ namespace Soenneker.Quark.Enums;
 /// An HTML CSS in a .NET enumeration form
 /// </summary>
 [EnumValue<string>]
+[IncludeEnumValues(typeof(GlobalKeyword))]
 public partial class FontStyleKeyword
 {
     /// <summary>
@@ -17,6 +18,4 @@ public partial class FontStyleKeyword
     /// Normal (non-italic) text.
     /// </summary>
     public static readonly FontStyleKeyword Normal = new("normal");
-
-    public static implicit operator FontStyleKeyword(GlobalKeyword style) => new(style.Value); // Value is the underlying string
 }
