fea0e87da2fd6d49ad6e4ed03e6c27d7839e8b1a
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:02:28 -0500

    TUnit conversion

--- test/Soenneker.Quark.Enums.FontStyles.Tests/FontStyleTests.cs
+++ test/Soenneker.Quark.Enums.FontStyles.Tests/FontStyleTests.cs
@@ -1,11 +1,10 @@
 using Soenneker.Tests.Unit;
-using Xunit;
 
 namespace Soenneker.Quark.Enums.FontStyles.Tests;
 
 public sealed class FontStyleTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
